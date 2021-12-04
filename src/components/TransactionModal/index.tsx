886e5068e9b646527bb26868c09ccfa865cbc7ab
Bruno Rodrigues <brunohrsilva1@gmail.com>
2021-12-04 17:47:51 -0300

    Add form structure and style transaction modal

--- src/components/TransactionModal/index.tsx
+++ src/components/TransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
 
 interface Props {
 	isOpen: boolean;
@@ -7,8 +8,21 @@ interface Props {
 
 export default function TransactionModal({ isOpen, onRequestClose }: Props) {
 	return (
-		<Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-			<h2>hey</h2>
+		<Modal
+			isOpen={isOpen}
+			onRequestClose={onRequestClose}
+			overlayClassName="react-modal-overlay"
+			className="react-modal-content"
+		>
+			<Container>
+				<h2>Cadastrar transação</h2>
+
+				<input type="text" placeholder="Título" />
+				<input type="number" placeholder="Valor" />
+				<input type="text" placeholder="Categoria" />
+
+				<button type="submit">Cadastrar</button>
+			</Container>
 		</Modal>
 	);
 }
